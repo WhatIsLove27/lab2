a24d19e425de5ea72c29ad4f4777d739fda886cc
WhatIsLove27 <60379901+WhatIsLove27@users.noreply.github.com>
2025-04-17 10:13:01 +0900

    updHome

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,9 @@ export default class Home extends Component {
             <h1>Главная страница</h1>
             <br />
             <p >Добро пожаловать на мой сайт!</p>
+            <div className="max-w-[400px] w-[100%] rounded-[10px] p-[25px] z-[1000] m-[auto] mt-[25px]">
+              <input name="name" type="text" className="w-full focus-within:outline-none border-2 px-2 bg-gray-800 border-gray-500 rounded-md" />
+            </div>
             <Button type="submit" className="mt-[25px] m-[auto]"  width="200px" height={'h-[35px]'}  text={'Перейти дальше'} />
 
         </div>
